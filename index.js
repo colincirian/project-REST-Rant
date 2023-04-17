6198dec6abad00cfe6a9b41a83616529981ed65e
Colin <colindcirian@gmail.com>
2023-04-16 22:13:54 -0700

    testing gitignore

---
+++ index.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+app.get('*', (req, res) => {
+    res.status(404).send("<h1>404 PAGE</h1>")
+})
+
+app.listen(process.env.PORT)
