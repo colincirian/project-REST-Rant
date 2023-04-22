4fd829c45f928e17784f6ffe9fbfe0fbacd3dbb1
Colin <colindcirian@gmail.com>
2023-04-21 19:23:38 -0700

    Jazzed up form page

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
-
-// Controllers & Routes
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
