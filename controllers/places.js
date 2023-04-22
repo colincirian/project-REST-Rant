f0394ac9989c739a0b3d0f2e112562f8d88d3431
Colin <colindcirian@gmail.com>
2023-04-22 14:59:15 -0700

    edit part

--- controllers/places.js
+++ controllers/places.js
@@ -23,6 +23,35 @@ router.get('/:id', (req, res) => {
   }
 })
 
+router.delete('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    places.splice(id, 1)
+    res.redirect('/places')
+  }
+})
+
+router.get('/:id/edit', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
+  }
+  else if (!places[id]) {
+      res.render('error404')
+  }
+  else {
+    res.render('places/edit', { place: places[id], id})
+  }
+})
+
+
+
 
 
 router.post('/', (req, res) => {
