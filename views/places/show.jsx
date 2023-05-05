5376a61c59fea14fad529162afccfa61ae4fea72
Colin <colindcirian@gmail.com>
2023-05-04 19:28:09 -0700

    LOTS OF PROGRESS

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,27 +1,51 @@
 const React = require("react");
 const Def = require("../default");
 
-function show(data) {
+function show (data) {
+  let comments = (
+    <h3 className="inactive">
+      No comments yet!
+    </h3>
+  )
+  if (data.place.comments.length) {
+    comments = data.place.comments.map(c => {
+      return (
+        <div className="border">
+          <h2 className="rant">{c.rant ? 'Rant! ðŸ˜¡' : 'Rave! ðŸ˜»'}</h2>
+          <h4>{c.content}</h4>
+          <h3>
+            <strong>- {c.author}</strong>
+          </h3>
+          <h4>Rating: {c.stars}</h4>
+        </div>
+      )
+    })
+  }
   return (
     <Def>
       <main>
-        <h1>{data.place.name}</h1>
-        <h2>Rating</h2>
-        <p>Not Rated</p>
-        <h2>Description</h2>
-        <h3>Located in {`${data.place.city}, ${data.place.state}`}</h3>
-        <h3>{data.place.showEstablished()}</h3>
-        <h4>Serving {data.place.cuisines}</h4>
-        <h4>Comments</h4>
-        <p>No comments yet!</p>
-        <a href={`/places/${data.id}/edit`} className="btn btn-warning">
-          Edit
-        </a>
-        <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
-          <button type="submit" className="btn btn-danger">
-            Delete
-          </button>
-        </form>
+        <div className="row">
+          <div className="col-sm-6">
+            <h1>{data.place.name}</h1>
+            <img src={data.place.pic} alt={data.place.name} />
+            <h3>Located in {`${data.place.city}, ${data.place.state}`}</h3>
+          </div>
+          <div className="col-sm-6">
+            <h2> Description </h2>
+            <h3> {data.place.showEstablished()}</h3>
+            <h4> Serving {data.place.cuisines}</h4>
+            <h2>Comments</h2>
+            {comments}
+            <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+              Edit
+            </a>
+            <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+              <button type="submit" className="btn btn-danger">
+                Delete
+              </button>
+            </form>
+          </div>
+        </div>
       </main>
     </Def>
   );
