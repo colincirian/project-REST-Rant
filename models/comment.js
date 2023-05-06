8e43423ccfed2b0515bb18bc612cacba84454ec7
Colin <colindcirian@gmail.com>
2023-05-06 12:35:38 -0700

    finally got everything running

--- models/comment.js
+++ models/comment.js
@@ -1,10 +1,10 @@
-require('dotenv').config()
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true, 
-  useUnifiedTopology: true
+let commentSchema = new mongoose.Schema({
+  author: { type: String, default: 'Anonymous' },
+  rant: { type: Boolean, default: false },
+  stars: { type: Number, required: true },
+  content: { type: String, default: '' }
 })
 
-module.exports.Place = require('./places')
-module.exports.Comment = require('./comment')
+module.exports = mongoose.model('Comment', commentSchema)
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -21,7 +21,6 @@ function index (data) {
     )
   })
   
-  
   return (
     <Def>
         <main>
@@ -34,5 +33,4 @@ function index (data) {
   )
 }
 
-  
 module.exports = index
